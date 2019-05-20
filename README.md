47c0660c848bca8ec017a37804261dafb9ea00ea
Dmitriy Smirnov <dreps350@gmail.com>
2019-05-21 00:51:04 +0300

    Fixed README.md, bot.py docstring

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 
 Формат записи: uid —> [audio_message_0, audio_message_1, ..., audio_message_N].
+
+За основу взят пример из библотеки python-telegram-bot
+
+https://github.com/python-telegram-bot/python-telegram-bot/blob/v11.1.0/examples/echobot2.py
--- bot.py
+++ bot.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """Simple Bot to reply to Telegram messages.
-This program is dedicated to the public domain under the CC0 license.
 This Bot uses the Updater class to handle the bot.
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
 Usage:
-Basic Echobot example, repeats messages.
+Stores voice message files to './content/<user_id>/' folder, where user_id is a message sender id.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
